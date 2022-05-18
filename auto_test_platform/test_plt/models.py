9d05f2bbafbf40e885d05e481d5517e740d143ce
Dechengcode <1293350946@qq.com>
2022-05-18 10:07:21 +0800

    新建内部类，测试项目

--- auto_test_platform/test_plt/models.py
+++ auto_test_platform/test_plt/models.py
@@ -4,4 +4,13 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name='测试项目名称')
+
+    # 默认显示
+    def __str__(self):
+        return self.name
+
+    # 新建一个内部类
+    class Meta:
+        verbose_name = '测试项目'
+        verbose_name_plural = verbose_name
