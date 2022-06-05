1bb558cf52d72b4d6c438756b5f71719676b7a3c
Dechengcode <1293350946@qq.com>
2022-06-05 08:14:48 +0800

    新增项目成员

--- auto_test_platform/test_plt/apps.py
+++ auto_test_platform/test_plt/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class TestPltConfig(AppConfig):
     # program object name
     name = 'test_plt'
-    verbose_name = '测试平台'
+    verbose_name = '项目管理'
 
 
--- auto_test_platform/test_plt/models.py
+++ auto_test_platform/test_plt/models.py
@@ -29,6 +29,9 @@ class Project(models.Model):
     # 最后更新时间
     updated_at = models.DateTimeField(auto_now=True, verbose_name='最近更新时间')
     # 项目成员
+    members = models.ManyToManyField(User, related_name='project_members',
+                                     through='ProjectMember',
+                                     through_fields=('project', 'user'))
     # 测试环境
 
     # 默认显示
@@ -37,5 +40,49 @@ class Project(models.Model):
 
     # 新建一个内部类
     class Meta:
-        verbose_name = '测试项目'
+        verbose_name = '项目设置'
+        verbose_name_plural = verbose_name
+
+
+class ProjectMember(models.Model):
+    """
+    项目成员(项目和用户之间的关系)
+    """
+    MemberRule = (
+        (1, '测试员'),
+        (2, '测试组长'),
+        (3, '测试经理'),
+        (4, '开发'),
+        (5, '运维'),
+        (6, '项目经理')
+    )
+    # 主键
+    id = models.AutoField(primary_key=True)
+    # 项目
+    project = models.ForeignKey(Project, on_delete=models.PROTECT, verbose_name='测试项目')
+    # 用户
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name='创建人')
+    # 加入日期
+    join_date = models.DateField(verbose_name='加入日期')
+    # 角色
+    role = models.IntegerField(choices=MemberRule, verbose_name='角色')
+    # 状态
+    status = models.BooleanField(default=True, verbose_name='状态')
+    # 退出日期
+    quit_date = models.DateField(verbose_name='退出日期')
+    # 备忘录
+    memo = models.CharField(max_length=200, verbose_name='备忘录', blank=True, null=True)
+
+    # 增加判空逻辑
+    def __str__(self):
+        if not self.user:
+            return '-'
+        else:
+            # 张某某
+            firstname = self.user.first_name if self.user.first_name else '-'
+            username = self.user.username
+            return f"{firstname}({username})"
+
+    class Meta:
+        verbose_name = '项目成员'
         verbose_name_plural = verbose_name
